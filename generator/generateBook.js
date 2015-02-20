9f45ade05c18a0b1652e9b4e7d690dcbc6113f9e
Sébastien CHOPIN <atinux@gmail.com>
2015-02-20 01:04:08 +0100

    Finished test

--- generator/generateBook.js
+++ generator/generateBook.js
@@ -29,7 +29,7 @@ var configService = {
 		textFont : 'Times-Roman',
 		boldFont : 'Times-Bold',
 		mounthSize : 28,
-		width: 382.5, // 135 mm = 5,4 * 2,5 cm = 5,4 * 1 inch = 5,4 * 72 points = 388,8
+		width: 383, // 135 mm = 5,4 * 2,5 cm = 5,4 * 1 inch = 5,4 * 72 points = 388,8
 		height: 539, // 190mm = 547,2 points + 20mm (57,6 pts) => 604,8
 		margin : 70, // 25mm = 45
 		alinea : 30
@@ -47,7 +47,7 @@ var configService = {
 		largeur: 389 // 13,5 cm = 5,4 * 2,5cm => 5,4 * 72 pts = 388.8
 	}
 };
-var idClient = 71;
+var idClient = 318;
 
 var constructClientFromCSV = function (idClient, success, failure) {
 	var csvDataIn = configService.workspace.csv_dir + idClient+'/data.csv' ;
@@ -79,17 +79,22 @@ var constructClientFromCSV = function (idClient, success, failure) {
 	}
 };
 var initBookOptions = function(client){
-    return {
-        idClient: client.idClient,
-        firstDates: [],
-        lastDates: [],
-        from: client.from,
-        to: client.to,
-        cover: client.cover,
-        emoji: client.emoji,
-        resume: client.resume,
-        pages: []
-    };
+	return {
+		idClient: client.idClient,
+		firstDates: [],
+		lastDates: [],
+		from: client.from,
+		to: client.to,
+		cover: client.cover,
+		emoji: client.emoji,
+		resume: client.resume,
+		pages: [],
+		sizes: {
+			width: configService.police.width,
+			height: configService.police.height,
+			margin: configService.police.margin,
+		},
+	};
 };
 
 var createDocForBook = function(index, bookOptions){
@@ -117,19 +122,19 @@ var createDocForBook = function(index, bookOptions){
 };
 
 var addFirstPage = function (doc, client) {
-    doc.moveDown(6)
-    .fontSize(configService.police.mounthSize)
-    .font(configService.police.boldFont)
-    .fillColor('#000000')
-    .text(client.to, {align:'center'})
-    .fillColor('#868786')
-    .text('& ' + client.from, {align:'center'})
-    .moveDown(3)
-    .image('images/logo.png', (configService.police.width * 0.3),doc.y, {width: configService.police.width * 0.4});
-    doc.image('images/1.png', configService.police.width/2 + 4, configService.police.height*0.95, {width:4}).addPage();
-    doc.image('images/2.png', configService.police.width/2 + 4, configService.police.height*0.95, {width:4}).addPage();
-    doc.image('images/3.png', configService.police.width/2 + 4, configService.police.height*0.95, {width:4}).addPage();
-    doc.image('images/4.png', configService.police.width/2 + 4, configService.police.height*0.95, {width:4});
+	doc.moveDown(6)
+	.fontSize(configService.police.mounthSize)
+	.font(configService.police.boldFont)
+	.fillColor('#000000')
+	.text(client.to, {align:'center'})
+	.fillColor('#868786')
+	.text('& ' + client.from, {align:'center'})
+	.moveDown(3)
+	.image('images/logo.png', (configService.police.width * 0.3),doc.y, {width: configService.police.width * 0.4});
+	doc.image('images/1.png', configService.police.width/2 + 4, configService.police.height*0.95, {width:4}).addPage();
+	doc.image('images/2.png', configService.police.width/2 + 4, configService.police.height*0.95, {width:4}).addPage();
+	doc.image('images/3.png', configService.police.width/2 + 4, configService.police.height*0.95, {width:4}).addPage();
+	doc.image('images/4.png', configService.police.width/2 + 4, configService.police.height*0.95, {width:4});
 };
 
 /*
@@ -142,13 +147,14 @@ var path = win.window.location.protocol + '//' + win.window.location.pathname.sl
 var pdf = require('html-pdf');
 var ejs = require('ejs');
 var async = require('async');
+var moment = require('moment');
 var fs = require('fs');
 var emojify = require('emojify.js');
 var emojiStrip = require('emoji-strip');
 var EmojiData = require('emoji-data');
-emojify.setConfig({
-	img_dir: path + '/images/emoji',
-});
+// emojify.setConfig({
+// 	img_dir: path + '/images/emoji',
+// });
 
 constructClientFromCSV(idClient, function (client) {
 	var csvName = configService.workspace.csv_dir + client.idClient+'/export.csv';
@@ -173,12 +179,23 @@ constructClientFromCSV(idClient, function (client) {
 				if (!ec) return;
 				message = message.replace(EmojiData.unified_to_char(ec.unified), ':'+ec.short_name+':');
 			});
-			message = decodeURI(emojify.replace(encodeURI(message).replace(/%EF%B8%8F/g, '')));
+			message = decodeURI(encodeURI(message).replace(/%EF%B8%8F/g, ''));
+			message = emojify.replace(message);
 		} else {
 			message = emojiStrip(message);
 		}
-		row[3] = message;
-		allMessages.push(row);
+		var date = moment(row[0], 'YYYY-MM-DD HH:mm:ss');
+		allMessages.push({
+			date: date.toISOString(),
+			month: date.format('YYYY-MM'),
+			day: date.format('YYYY-MM-DD'),
+			dateDisplay: date.format('D') + ' ' + configService.MONTH[date.month()] + ' ' + date.year(),
+			monthDisplay: configService.MONTH[date.month()] + ' ' + date.year(),
+			dayDisplay: date.format('D') + ' ' +configService.MONTH[date.month()],
+			from: row[1],
+			to: row[2],
+			body: message
+		});
 	})
 	.on('end', function (count) {
 		// Generate books
@@ -186,20 +203,24 @@ constructClientFromCSV(idClient, function (client) {
 		async.timesSeries(nbBooks, function (i, next) {
 			console.log('Generate book ['+i+']');
 			var messages = allMessages.slice(i * smsPerBook, i * smsPerBook + smsPerBook);
-			bookOptions.firstDates[i] = new Date().setTime(Date.parse(messages[0]));
+			bookOptions.firstDates[i] = messages[0].dateDisplay;
+			bookOptions.lastDates[i] = messages[messages.length - 1].dateDisplay;
 			var html = ejs.render(template, {
 				client: client,
 				book: bookOptions,
 				messages: messages,
+				path: path,
 			});
 			var filename = configService.workspace.pdf_dir + bookOptions.idClient + '/Client'+bookOptions.idClient+'-' + (i + 1) + '.pdf';
 			$('body').html(html);
-			html = html.replace(/css\/(.+)\.css/g, path + '/css/$1.css'); // Absolute path for CSS
-			html = html.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, ''); // Remove scripts
+			// html = html.replace(/css\/(.+)\.css/g, path + '/css/$1.css'); // Absolute path for CSS
+			// html = html.replace(/js\/libs\/(.+)\.js/g, path + '/js/libs/$1.js'); // Absolute path for JS
+			// html = html.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, ''); // Remove scripts
 			var options = {
 				// Papersize Options: http://phantomjs.org/api/webpage/property/paper-size.html
-				width: '135mm', // allowed units: mm, cm, in, px
-				height: '190mm', // allowed units: mm, cm, in, px
+				// width: (configService.police.width + configService.police.margin) + 'px', // 135mm, allowed units: mm, cm, in, px
+				width: '135mm', // 135mm, allowed units: mm, cm, in, px
+				height: '190mm', // 190mm, allowed units: mm, cm, in, px
 				orientation: 'portrait', // or landscape
 				// Page options
 				border: '5mm', // default is 0, units: mm, cm, in, px
@@ -208,79 +229,27 @@ constructClientFromCSV(idClient, function (client) {
 				// 	contents: '<div style="text-align: center;border-bottom: 1px #ddd solid;">Author: Chopin brothers</div>',
 				// },
 				footer: {
-					height: '7mm',
+					height: '5mm',
 					contents: '<div style="text-align: center;font-size: 12px;">{{page}}</div>',
 				},
 				// File options
 				type: 'pdf',
 				quality: 100,
 				// Export options
-				filename: filename
+				filename: filename,
+				script: './generator/htmlToPDF.js',
 			};
 			pdf.create(html, options, function (err, res) {
-				console.log(err, res);
-	            // win.window.open(filename);
+				console.log(err, (res && !res.filename ? res.toString() : res));
+				bookOptions.pages[i] = res.pages || 0;
+				// win.window.open(filename);
 				next();
 			});
-			// addFirstPage(doc,client);
-			// recordDate.setTime(Date.parse(row[0]));
-			// if (isMounthChanged(oldDate,recordDate)) {
-			// 	addMounth(doc,recordDate);
-			// }	
-			// if (dayIsChanged(oldDate,recordDate)) {
-			// 	addDay(doc,recordDate);
-			// }
-			// oldDate = new Date();
-			// oldDate.setTime(recordDate.getTime());
-			// var emoSms = (client.emoji === 'oui') ? emojify.replace(decodeURIComponent(row[3])) : decodeURIComponent(row[3]);
-			// if (client.from === decodeURIComponent(row[1])) {
-			// 	addSMS(doc, emoSms, '#868786', 'right');
-			// } else {
-			// 	addSMS(doc, emoSms, '#000000', 'left');
-			// }
-			// SMSCounter++;
-			// currentPageNumber = doc.bufferedPageRange().count;
-			// if (currentPageNumber !== 1 && currentPageNumber !== previousPageNumber) {
-			// 	addPageNumber(doc, currentPageNumber);
-			// 	previousPageNumber = currentPageNumber;
-			// }
-			// bookOptions.lastDates[currentBook] = recordDate;
-			// bookOptions.pages[currentBook] =  doc.bufferedPageRange().count;
-			// firstPage = false;
-			// if (SMSCounter > smsPerBook && currentBook < nbBooks -1) {
-			// 	var lastDateForCurrentBook = new Date();
-			// 	lastDateForCurrentBook.setTime(recordDate.getTime());
-			// 	bookOptions.lastDates[currentBook] = lastDateForCurrentBook;
-			// 	doc.addPage();
-			// 	addPageNumber(doc, currentPageNumber + 1);
-			// 	doc.addPage();
-			// 	addPageNumber(doc, currentPageNumber + 2);
-			// 	doc.addPage();
-			// 	addPageNumber(doc, currentPageNumber + 3);
-			// 	bookOptions.pages[currentBook] =  doc.bufferedPageRange().count;
-			// 	doc.end();
-			// 	currentBook++;
-			// 	doc = docs[currentBook];
-			// 	oldDate= null;
-			// 	SMSCounter = 0;
-			// 	firstPage = true;
-			// }
 		}, function (err, books) {
 			console.log('Finished', err, books);
 		});
-		return;
-
-		doc.addPage();
-		addPageNumber(doc, currentPageNumber + 1);
-		doc.addPage();
-		addPageNumber(doc, currentPageNumber + 2);
-		doc.addPage();
-		addPageNumber(doc, currentPageNumber + 3);
-		bookOptions.pages[currentBook] =  doc.bufferedPageRange().count;
-		doc.end();
-		writeBooks(nbBooks, bookOptions);
-		setTimeout(function(){ success(nbBooks); }, 1000);
-	}).on('error', function(error){
+	})
+	.on('error', function(error){
 		console.log(error.message);
 	});
 }, console.log.bind(console, 'Error'));
